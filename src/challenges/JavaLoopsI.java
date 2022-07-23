a0d27d98ce0efb7653fe9064a90d3c6194cd5eb7
Tiago Santos <pereirat476@gmail.com>
2022-07-23 11:29:18 -0300

    Java Loops I

---
+++ src/challenges/JavaLoopsI.java
@@ -0,0 +1,16 @@
+package challenges;
+
+import java.util.Scanner;
+
+public class JavaLoopsI {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        if(N>=2||N<=20){
+            for(int i=1;i<=10;i++){
+                System.out.println(N+" x "+i+" = "+N*i );
+            }
+        }
+    }
+
+}
